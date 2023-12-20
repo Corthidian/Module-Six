b8765765b90d6dfa074712fbd6679e2577bbf03f
Corthidian <lucifer880799@gmail.com>
2023-12-20 16:16:06 +0500

    writing some of the logic for the colorsutil.c file

--- colorUtils.c
+++ colorUtils.c
@@ -9,5 +9,34 @@ typedef enum
     INVALID_INPUT
 } ErrorCode;
 
+int toGrayScale(int *r, int *g, int*b, Mode m)
+ {
+    ErrorCode error = NO_ERROR;
+if(r == NULL|| g == NULL || b == NULL) 
+{
+    return
+ INVALID_INPUT;
+}
 
+int grayValue;
+
+    
+switch(m) 
+{
+        
+case AVERAGE:grayValue = (*r + *g + *b) / 3;
+            
+break;
+        
+case LIGHTNESS: grayValue = (*r + *g + *b) / 3;
+
+break;
+        
+case LUMINOSITY: grayValue = (int)round(0.21 * *r + 0.72 * *g + 0.07* *b); 
+            
+break;
+        
+default: error = INVALID_INPUT;
+
+}
 
